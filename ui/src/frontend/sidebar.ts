8e9278a00360f790f32911e73043246ba5c63c5d
Clark Gaebel <leer.karats0p@icloud.com>
2022-03-29 16:10:36 -0400

    Rework the sidebar
    remove legacy ui
    remove record new trace
    
    remove the ability to download a trace
    remove metrics
    remove info and stats
    remove trace conversion
    
    add an example ocaml trace
    add an example C trace
    
    make Documentation point to the readme
    remove flags
    make "Report a bug" go to the magic-trace issue tracker
    add an About link that acknowledges Perfetto
    
    remove sample queries

--- ui/src/frontend/sidebar.ts
+++ ui/src/frontend/sidebar.ts
@@ -16,8 +16,8 @@ import * as m from 'mithril';
 
 import {assertExists, assertTrue} from '../base/logging';
 import {Actions} from '../common/actions';
-import {getCurrentChannel} from '../common/channels';
 import {TRACE_SUFFIX} from '../common/constants';
+import { getCurrentChannel } from '../common/channels';
 import {ConversionJobStatus} from '../common/conversion_jobs';
 import {Engine} from '../common/engine';
 import {featureFlags} from '../common/feature_flags';
@@ -36,11 +36,10 @@ import {showModal} from './modal';
 import {Router} from './router';
 import {isDownloadable, isShareable} from './trace_attrs';
 import {
-  convertToJson,
-  convertTraceToJsonAndDownload,
-  convertTraceToSystraceAndDownload,
+  convertToJson
 } from './trace_converter';
 
+/*
 const ALL_PROCESSES_QUERY = 'select name, pid from process order by name;';
 
 const CPU_TIME_FOR_PROCESSES = `
@@ -106,6 +105,7 @@ select
     query
 from sqlstats, first
 order by started desc`;
+*/
 
 const GITILES_URL =
     'https://android.googlesource.com/platform/external/perfetto';
@@ -113,11 +113,7 @@ const GITILES_URL =
 let lastTabTitle = '';
 
 function getBugReportUrl(): string {
-  if (globals.isInternalUser) {
-    return 'https://goto.google.com/perfetto-ui-bug';
-  } else {
-    return 'https://github.com/google/perfetto/issues/new';
-  }
+  return 'https://github.com/janestreet/magic-trace/issues';
 }
 
 const HIRING_BANNER_FLAG = featureFlags.register({
@@ -131,6 +127,7 @@ function shouldShowHiringBanner(): boolean {
   return globals.isInternalUser && HIRING_BANNER_FLAG.get();
 }
 
+/*
 function createCannedQuery(query: string): (_: Event) => void {
   return (e: Event) => {
     e.preventDefault();
@@ -158,6 +155,11 @@ const EXAMPLE_ANDROID_TRACE_URL =
 
 const EXAMPLE_CHROME_TRACE_URL =
     'https://storage.googleapis.com/perfetto-misc/example_chrome_trace_4s_1.json';
+*/
+
+
+const EXAMPLE_OCAML_TRACE_URL = globals.root + "assets/ocaml.ftf"
+const EXAMPLE_C_TRACE_URL = globals.root + "assets/c.ftf"
 
 interface SectionItem {
   t: string;
@@ -188,12 +190,14 @@ const SECTIONS: Section[] = [
     expanded: true,
     items: [
       {t: 'Open trace file', a: popupFileSelectionDialog, i: 'folder_open'},
+      /*
       {
         t: 'Open with legacy UI',
         a: popupFileSelectionDialogOldUI,
         i: 'filter_none',
       },
       {t: 'Record new trace', a: navigateRecord, i: 'fiber_smart_record'},
+      */
     ],
   },
 
@@ -213,18 +217,23 @@ const SECTIONS: Section[] = [
         isPending: () => globals.getConversionJobStatus('create_permalink') ===
             ConversionJobStatus.InProgress,
       },
+      /*
       {
         t: 'Download',
         a: downloadTrace,
         i: 'file_download',
         checkDownloadDisabled: true,
       },
+      */
       {t: 'Query (SQL)', a: navigateAnalyze, i: 'control_camera'},
+      /*
       {t: 'Metrics', a: navigateMetrics, i: 'speed'},
       {t: 'Info and stats', a: navigateInfo, i: 'info'},
+      */
     ],
   },
 
+  /*
   {
     title: 'Convert trace',
     summary: 'Convert to other formats',
@@ -259,12 +268,14 @@ const SECTIONS: Section[] = [
 
     ],
   },
+  */
 
   {
     title: 'Example Traces',
     expanded: true,
     summary: 'Open an example trace',
     items: [
+      /*
       {
         t: 'Open Android example',
         a: openTraceUrl(EXAMPLE_ANDROID_TRACE_URL),
@@ -275,6 +286,17 @@ const SECTIONS: Section[] = [
         a: openTraceUrl(EXAMPLE_CHROME_TRACE_URL),
         i: 'description',
       },
+      */
+      {
+        t: 'OCaml',
+        a: openTraceUrl(EXAMPLE_OCAML_TRACE_URL),
+        i: 'description'
+      },
+      {
+        t: 'C',
+        a: openTraceUrl(EXAMPLE_C_TRACE_URL),
+        i: 'description'
+      },
     ],
   },
 
@@ -284,16 +306,24 @@ const SECTIONS: Section[] = [
     summary: 'Documentation & Bugs',
     items: [
       {t: 'Keyboard shortcuts', a: openHelp, i: 'help'},
-      {t: 'Documentation', a: 'https://perfetto.dev', i: 'find_in_page'},
+      { t: 'Documentation', a: 'https://github.com/janestreet/magic-trace', i: 'find_in_page' },
+      /*
       {t: 'Flags', a: navigateFlags, i: 'emoji_flags'},
+      */
       {
         t: 'Report a bug',
         a: () => window.open(getBugReportUrl()),
         i: 'bug_report',
       },
+      {
+        t: 'About',
+        a: 'https://github.com/janestreet/magic-trace/wiki/About-the-UI',
+        i: 'info'
+      }
     ],
   },
 
+  /*
   {
     title: 'Sample queries',
     summary: 'Compute summary statistics',
@@ -348,6 +378,7 @@ const SECTIONS: Section[] = [
       },
     ],
   },
+  */
 
 ];
 
@@ -367,11 +398,13 @@ function popupFileSelectionDialog(e: Event) {
   getFileElement().click();
 }
 
+/*
 function popupFileSelectionDialogOldUI(e: Event) {
   e.preventDefault();
   getFileElement().dataset['useCatapultLegacyUi'] = '1';
   getFileElement().click();
 }
+*/
 
 function downloadTraceFromUrl(url: string): Promise<File> {
   return m.request({
@@ -410,6 +443,7 @@ export async function getCurrentTrace(): Promise<Blob> {
   }
 }
 
+/*
 function openCurrentTraceWithOldUI(e: Event) {
   e.preventDefault();
   assertTrue(isTraceLoaded());
@@ -451,6 +485,7 @@ function convertTraceToJson(e: Event) {
         throw new Error(`Failed to get current trace ${error}`);
       });
 }
+*/
 
 export function isTraceLoaded(): boolean {
   return globals.getCurrentEngine() !== undefined;
@@ -536,16 +571,19 @@ function openInOldUIWithSizeCheck(trace: Blob) {
   return;
 }
 
+/*
 function navigateRecord(e: Event) {
   e.preventDefault();
   Router.navigate('#!/record');
 }
+*/
 
 function navigateAnalyze(e: Event) {
   e.preventDefault();
   Router.navigate('#!/query');
 }
 
+/*
 function navigateFlags(e: Event) {
   e.preventDefault();
   Router.navigate('#!/flags');
@@ -560,6 +598,7 @@ function navigateInfo(e: Event) {
   e.preventDefault();
   Router.navigate('#!/info');
 }
+*/
 
 function navigateViewer(e: Event) {
   e.preventDefault();
@@ -717,7 +756,6 @@ async function finaliseMetatrace(e: Event) {
 
   downloadUrl(url, 'metatrace');
 }
-
 
 const EngineRPCWidget: m.Component = {
   view() {
