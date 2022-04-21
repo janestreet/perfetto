b8c050ea677c119172ea7e211b6f0ba63a46903a
Clark Gaebel <leer.karats0p@icloud.com>
2022-04-20 20:23:03 -0400

    Make examples come up when users click on them
    This has two parts:
    
    1. Show the timeline as soon as a trace is loaded.
    2. Expand all the tracks as on initial load.
    
    Fixes #16
    Fixes #17

--- ui/src/controller/track_decider.ts
+++ ui/src/controller/track_decider.ts
@@ -467,7 +467,7 @@ class TrackDecider {
       summaryTrackId,
       name: MEM_DMA_COUNTER_NAME,
       id,
-      collapsed: true,
+      collapsed: false,
     });
     this.addTrackGroupActions.push(addGroup);
   }
@@ -637,7 +637,7 @@ class TrackDecider {
         summaryTrackId: groupIds.summaryTrackId,
         name: groupName,
         id: groupIds.id,
-        collapsed: true,
+        collapsed: false,
       });
       this.addTrackGroupActions.push(addGroup);
     }
@@ -1423,7 +1423,6 @@ class TrackDecider {
       const threadName = it.threadName;
       const processName = it.processName;
       const hasSched = !!it.hasSched;
-      const hasHeapProfiles = !!it.hasHeapProfiles;
 
       const labels = [];
       if (it.argSetId !== null) {
@@ -1475,7 +1474,7 @@ class TrackDecider {
           // Perf profiling tracks remain collapsed, otherwise we would have too
           // many expanded process tracks for some perf traces, leading to
           // jankyness.
-          collapsed: !hasHeapProfiles,
+          collapsed: false,
         });
 
         this.addTrackGroupActions.push(addTrackGroup);
