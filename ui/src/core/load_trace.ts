55532069ba505340b8972648476d1fe00c87a49b
Tristan Hume <tris.hume@gmail.com>
2024-10-21 04:08:40 -0400

    Enable serving without https, without caches
    window.caches is only available when served under
    a bunch of conditions including https. Sometimes
    we may want to host a local Perfetto UI without a
    certificate so make this optional.
    
    Co-authored-by: Tudor Brindus <tbrindus@janestreet.com>
    Signed-off-by: Tudor Brindus <tbrindus@janestreet.com>

--- ui/src/core/load_trace.ts
+++ ui/src/core/load_trace.ts
@@ -271,7 +271,9 @@ async function loadTraceIntoEngine(
   // Plugins may call trace.initialPage.suggest(...) during onTraceLoad to
   // request that the app navigate somewhere other than /viewer.
   const initialRoute = trace.initialPage.getWinner() ?? '/viewer';
-  Router.navigate(`#!${initialRoute}?local_cache_key=${cacheUuid}`);
+  if (cacheUuid && cacheUuid != '') {
+    Router.navigate(`#!${initialRoute}?local_cache_key=${cacheUuid}`);
+  }
 
   decideTabs(trace);
 
